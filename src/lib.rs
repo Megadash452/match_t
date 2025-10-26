8a4d9958587df7d08a9b63185e3e957f1b989488
Megadash452 <megadash452@proton.me>
2025-10-26 17:36:00 -0400

    Ran cargo fmt and clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,39 @@
 // TODO: document crate
 use proc_macro2::{Punct, Span, TokenStream};
-use quote::{quote, quote_spanned, ToTokens, TokenStreamExt as _};
-use syn::{braced, parse::{discouraged::Speculative as _, Parse, ParseStream}, punctuated::Punctuated, spanned::Spanned as _, token::Brace, Block, Expr, Ident, Token, Type};
+use quote::{ToTokens, TokenStreamExt as _, quote, quote_spanned};
+use syn::{
+    Block, Expr, Ident, Token, Type, braced,
+    parse::{Parse, ParseStream, discouraged::Speculative as _},
+    punctuated::Punctuated,
+    spanned::Spanned as _,
+    token::Brace,
+};
 
 /// Run different of code depending on the **concrete type** of `T`.
-/// 
+///
 /// This is essentially an `if/match` statement for *generic types*.
 /// Callers can use `match` with `T` as the *input* and the *cases* are **concrete types**.
 /// Callers can also use `if-else` statements with the `is` meta-keyword.
-/// 
+///
 /// Blocks of code are given the metavariable `$T`,
-/// which resolves to the 
-/// 
+/// which resolves to the
+///
 /// ## Limitations
-/// 
+///
 /// * The given type `T` can only be compared with **concrete types** (e.g. `i32`).
 ///   This macro can't check if `T` implements some trait because that is not possible in Rust (afaik).
-/// <br><br>
+///   <br><br>
 /// * The `$T` metavariable can't be used within `else` blocks or *default case* (`_` in match).
 ///   To assign a *type* to the metavariable, the macro checks each type comparison individually,
 ///   cloning the block for ***each type*** that `T` is compared with.
-/// 
+///
 ///   With this in mind, the macro will put blocks that *don't* use `$T` to optimize code size,
 ///   but you should still try to keep each block's size as *short as possible*.
-/// 
+///
 /// ## Example
-/// 
+///
 /// The following two examples output identical code:
-/// 
+///
 /// With `if-else` statement:
 /// ```
 /// # use match_t::match_t;
@@ -41,7 +47,7 @@ use syn::{braced, parse::{discouraged::Speculative as _, Parse, ParseStream}, pu
 ///     }
 /// }
 /// ```
-/// 
+///
 /// With `match` statement:
 /// ```
 /// # use match_t::match_t;
@@ -57,7 +63,7 @@ use syn::{braced, parse::{discouraged::Speculative as _, Parse, ParseStream}, pu
 pub fn match_t(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     match syn::parse::<MatchT>(input) {
         Ok(t) => t.into_token_stream().into(),
-        Err(error) => error.into_compile_error().into()
+        Err(error) => error.into_compile_error().into(),
     }
 }
 
@@ -66,11 +72,13 @@ pub fn match_t(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 /// Called by [`match_t`].
 enum MatchT {
     If(If),
-    Match(Match)
+    Match(Match),
 }
 impl Parse for MatchT {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        input.parse::<If>().map(Self::If)
+        input
+            .parse::<If>()
+            .map(Self::If)
             .or_else(|_| input.parse::<Match>().map(Self::Match))
     }
 }
@@ -90,7 +98,7 @@ struct If {
     condition: Condition,
     block: Block,
     else_ifs: Vec<ElseIf>,
-    else_stmnt: Option<Else>
+    else_stmnt: Option<Else>,
 }
 struct IsToken(Ident);
 struct ElseIf {
@@ -126,7 +134,12 @@ impl Parse for If {
                     // Encountered else
                     let new = input.parse::<Else>()?;
                     match else_stmnt {
-                        Some(_) => return Err(syn::Error::new(new.else_token.span, "Two 'else' statements are not allowed")),
+                        Some(_) => {
+                            return Err(syn::Error::new(
+                                new.else_token.span,
+                                "Two 'else' statements are not allowed",
+                            ));
+                        }
                         None => else_stmnt = Some(new),
                     }
                 }
@@ -135,7 +148,15 @@ impl Parse for If {
             }
         }
 
-        Ok(Self { if_token, t, is_token, condition, block, else_ifs, else_stmnt })
+        Ok(Self {
+            if_token,
+            t,
+            is_token,
+            condition,
+            block,
+            else_ifs,
+            else_stmnt,
+        })
     }
 }
 impl Parse for IsToken {
@@ -162,19 +183,25 @@ impl Parse for ElseIf {
 }
 impl Parse for Else {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        Ok(Self { else_token: input.parse()?, block: input.parse()? })
+        Ok(Self {
+            else_token: input.parse()?,
+            block: input.parse()?,
+        })
     }
 }
 impl ToTokens for If {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         self.if_token.to_tokens(tokens);
-        self.condition.to_tokens(&self.t, self.is_token.0.span(), tokens);
+        self.condition
+            .to_tokens(&self.t, self.is_token.0.span(), tokens);
         self.block.to_tokens(tokens);
 
         for else_if in &self.else_ifs {
             else_if.else_token.to_tokens(tokens);
             else_if.if_token.to_tokens(tokens);
-            else_if.condition.to_tokens(&else_if.t, else_if.is_token.0.span(), tokens);
+            else_if
+                .condition
+                .to_tokens(&else_if.t, else_if.is_token.0.span(), tokens);
             else_if.block.to_tokens(tokens);
         }
 
@@ -201,13 +228,13 @@ enum MatchBody {
     Block {
         block: Block,
         /// Match arm with braces can **skip** comma.
-        comma: Option<Token![,]>
+        comma: Option<Token![,]>,
     },
     Expr {
         expr: Expr,
         /// Match arm with braces **requires** a comma.
-        comma: Token![,]
-    }
+        comma: Token![,],
+    },
 }
 impl Parse for Match {
     fn parse(input: ParseStream) -> syn::Result<Self> {
@@ -221,26 +248,28 @@ impl Parse for Match {
         while !match_body.is_empty() {
             // When the case arm is just `_`, that goes in the default_case_arm.
             let fork = match_body.fork();
-            if let Ok(ident) = fork.parse::<Ident>() {
-                if ident == "_" {
-                    match_body.advance_to(&fork);
+            if let Ok(ident) = fork.parse::<Ident>() && ident == "_" {
+                match_body.advance_to(&fork);
 
-                    default_case_arm = Some((
-                        match_body.parse()?,
-                        match_body.parse()?
-                    ));
+                default_case_arm = Some((match_body.parse()?, match_body.parse()?));
 
-                    // default_case must also be the LAST case
-                    if !match_body.is_empty() {
-                        return Err(match_body.error("Unexpected tokens: Default case must be the last case"));
-                    }
+                // default_case must also be the LAST case
+                if !match_body.is_empty() {
+                    return Err(match_body
+                        .error("Unexpected tokens: Default case must be the last case"));
                 }
             }
             // Parse normal case
             arms.push(match_body.parse()?);
         }
 
-        Ok(Self { match_token, t, braces, arms, default_case_arm })
+        Ok(Self {
+            match_token,
+            t,
+            braces,
+            arms,
+            default_case_arm,
+        })
     }
 }
 impl Parse for MatchArm {
@@ -255,9 +284,15 @@ impl Parse for MatchArm {
 impl Parse for MatchBody {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         if input.peek(Brace) {
-            Ok(Self::Block { block: input.parse()?, comma: input.parse()? })
+            Ok(Self::Block {
+                block: input.parse()?,
+                comma: input.parse()?,
+            })
         } else {
-            Ok(Self::Expr { expr: input.parse()?, comma: input.parse()? })
+            Ok(Self::Expr {
+                expr: input.parse()?,
+                comma: input.parse()?,
+            })
         }
     }
 }
@@ -267,7 +302,7 @@ impl MatchBody {
             Self::Block { block, comma } => {
                 block.to_tokens(tokens);
                 comma.to_tokens(tokens);
-            },
+            }
             Self::Expr { expr, comma } => {
                 // If statement block must have braces, so add them if they are not in the arm's body.
                 default_braces.surround(tokens, |tokens| expr.to_tokens(tokens));
@@ -303,7 +338,7 @@ impl ToTokens for Match {
 struct Condition(Punctuated<Type, Token![|]>);
 impl Condition {
     /// Generate a `boolean` [`Condition`] that compares `T` with all [`Types`][Type] in `Self` for use in an `if` statement.
-    /// 
+    ///
     /// **eq_span** is the [`Span`] that will be assigned to the *equal signs* (`==`) in the condition.
     fn to_tokens(&self, generic_t: &Type, eq_span: Span, tokens: &mut TokenStream) {
         let type_of_t = type_of_tokens(generic_t);
@@ -316,8 +351,9 @@ impl Condition {
         };
 
         for pair in self.0.pairs() {
-            let type_of_ty = type_of_tokens(*pair.value());
-            let or = pair.punct() // Will output no tokens when None (aka at end)
+            let type_of_ty = type_of_tokens(pair.value());
+            let or = pair
+                .punct() // Will output no tokens when None (aka at end)
                 .map(|bitor| {
                     let mut first = Punct::new('|', proc_macro2::Spacing::Joint);
                     let mut second = Punct::new('|', proc_macro2::Spacing::Alone);
@@ -325,7 +361,7 @@ impl Condition {
                     second.set_span(bitor.span());
                     quote!(#first #second)
                 });
-            
+
             tokens.append_all(quote! { #type_of_t #eq #type_of_ty #or });
         }
     }
