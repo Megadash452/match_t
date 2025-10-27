2e728b907a8db93a1556fdaaf8dc335fba9e07d4
Megadash452 <megadash452@proton.me>
2025-10-26 22:26:32 -0400

    Update documentation to better represent the use of metavariable `$T`.

--- src/lib.rs
+++ src/lib.rs
@@ -16,37 +16,42 @@ use syn::parse::{Parse, ParseStream};
 /// This is essentially an `if/match` statement for *generic types*.
 /// Callers can use `match` with `T` as the *input* and the *cases* are **concrete types**.
 /// Callers can also use `if-else` statements with the `is` meta-keyword.
+/// 
+/// ## Metavariable
 ///
-/// Blocks of code are given the metavariable `$T`,
-/// which resolves to the
+/// Blocks of code are given the metavariable `$T`.
+/// The `T` in this case is the same *generic type* that was passed in.
+/// The metavariable resolves to the **concrete type** that matched with the type of `T`.
+/// 
+/// The metavariable is not available within the `else` block of the *`if`` statement*,
+/// or within the *default/catch-all* (`_`) arm of the *`match` statement*.
 ///
 /// ## Limitations
 ///
 /// * The given type `T` can only be compared with **concrete types** (e.g. `i32`).
 ///   This macro can't check if `T` implements some trait because that is not possible in Rust (afaik).
 ///   <br><br>
-/// * The `$T` metavariable can't be used within `else` blocks or *default case* (`_` in match).
-///   To assign a *type* to the metavariable, the macro checks each type comparison individually,
+/// * To assign a **concrete type** to the metavariable `$T`,
+///   the macro checks each type comparison individually,
 ///   cloning the block for ***each type*** that `T` is compared with.
-///
 ///   With this in mind, the macro will put blocks that *don't* use `$T` to optimize code size,
 ///   but you should still try to keep each block's size as *short as possible*.
 ///
 /// ## Example
 ///
-/// The following two examples output identical code:
+/// The following two examples produce identical code:
 ///
 /// With `if-else` statement:
 /// ```
 /// # use match_t::match_t;
-/// fn my_fn<T>() {
+/// fn my_fn<T: Any>() {
 ///     match_t! {
 ///         if T is bool | char | u8 | u32 | u64 | usize | u128 {
-///             println!("T is unsigned!")
+///             println!("T is unsigned :(")
 ///         } else if T is i8 | i32 | i64 | isize | i128 {
-///             println!("T is signed!")
+///             println!("T is signed! :) Absolute value of -6: {}", $T::abs(-6))
 ///         } else {
-///             println!("T is... something else: {}", std::any::type_name::<$T>())
+///             println!("T is... something else: {}", std::any::type_name::<T>())
 ///         }
 ///     }
 /// }
@@ -55,12 +60,12 @@ use syn::parse::{Parse, ParseStream};
 /// With `match` statement:
 /// ```
 /// # use match_t::match_t;
-/// fn my_fn<T>() {
+/// fn my_fn<T: Any>() {
 ///     match_t! {
 ///         match T {
-///             bool | char | u8 | u32 | u64 | usize | u128 => println!("T is unsigned!"),
-///             i8 | i32 | i64 | isize | i128 => { println!("T is signed!") }
-///             _ => println!("T is... something else: {}", std::any::type_name::<$T>())
+///             bool | char | u8 | u32 | u64 | usize | u128 => println!("T is unsigned :("),
+///             i8 | i32 | i64 | isize | i128 => { println!("T is signed! :) Absolute value of -6: {}", $T::abs(-6)) }
+///             _ => println!("T is... something else: {}", std::any::type_name::<T>())
 ///         }
 ///     }
 /// }
